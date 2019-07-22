7623781efe680bebb81a69a86facbf3e541e87b6
IvanC <ivancavdar25@gmail.com>
2019-07-22 13:09:33 +0300

    Add schedule

--- resources/views/pages/schedule.blade.php
+++ resources/views/pages/schedule.blade.php
@@ -3,26 +3,7 @@
 @section('title', __('Лицей им. А.С.Пушкина'))
 
 @section('content')
-    <main class="page-main">
-            <div class="schedule__table-wrap">
-                <table class="schedule">
-                    <thead class="schedule__head">
-                        <tr class="schedule__row">
-                            <th class=class="schedule__cell">--</th>
-                            <th class=class="schedule__cell">Понедельник</th>
-                            <th class=class="schedule__cell">Вторник</th>
-                            <th class=class="schedule__cell">Среда</th>
-                            <th class=class="schedule__cell">Четверг</th>
-                            <th class=class="schedule__cell">Пятница</th>
-                        </tr>
-                    </thead>
-                    <tbody class="schedule__body">
-                        
-                    </tbody>
-                </table>
-            </div>
-
-    </main>
+    @include('blocks.schedule')
 @endsection
 
 @section('footer')
