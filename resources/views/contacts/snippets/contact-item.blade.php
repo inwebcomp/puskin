077a92b37d8000898fef541691da647de0a9d11c
IvanC <ivancavdar25@gmail.com>
2019-07-31 09:14:42 +0300

    Adapt contacts

--- resources/views/contacts/snippets/contact-item.blade.php
+++ resources/views/contacts/snippets/contact-item.blade.php
@@ -2,15 +2,15 @@
     <p class="contact-item__title"> @lang('Директор')</p>
     <p class="contact-item__name"> @lang('Банарь Олеся Владиславовна') </p>
 
-    <a href="#" class="contact-item__phone">
+    <a href="tel:0 297 21 998" class="contact-item__phone">
         <i class="fas fa-phone contact-item__icon"></i>
         0 (297) 21 998
     </a>
-    <a href="#" class="contact-item__phone">
+    <a href="tel:0 60 15 77 71" class="contact-item__phone">
         <i class="far fa-mobile contact-item__icon"></i>
         0 (297) 21 998
     </a>
-    <a href="#" class="contact-item__phone">
+    <a href="mailto:ltbas1@mail.ru" class="contact-item__phone">
         <i class="far fa-at contact-item__icon"></i>
         0 (297) 21 998
     </a>
